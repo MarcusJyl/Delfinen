6b3ef386148cea9df2d46db38057def95b729203
Bloom9900 <jannichhoejmose@gmail.com>
2019-11-26 14:49:25 +0100

    Jannich

---
+++ Delfinen/src/delfinen/View/MainMenuUI.java
@@ -0,0 +1,14 @@
+package delfinen.View;
+
+public class MainMenuUI {
+    public static void showMainMenu() {
+        System.out.println("************************************************");
+        System.out.println("Velkommen til Delfinen svømmeklub");
+        System.out.println("Muligheder:");
+        System.out.println("1. Opret nyt medlem");
+        System.out.println("2. TBM");
+        System.out.println("3. TBM");
+        System.out.println("4. TBM");
+        System.out.println("************************************************");
+    }
+}
