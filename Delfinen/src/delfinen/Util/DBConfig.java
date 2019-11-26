a0edaed93220b002377c076d45e476c4e7e1e956
Marc <marc.g.ekstrom@gmail.com>
2019-11-26 15:15:09 +0100

    pass

--- Delfinen/src/delfinen/Util/DBConfig.java
+++ Delfinen/src/delfinen/Util/DBConfig.java
@@ -4,6 +4,6 @@ package delfinen.Util;
 
 public class DBConfig {
     static String username = "root";
-    static String password = "Kongdane4114956";
+    static String password = "Dug83cmn";
 
 }
