ea6e7d63d3374c4f496f4c02890211593c6e403f
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2019-11-28 13:46:31 +0100

    result

--- Delfinen/src/delfinen/Datamappers/DBResultat.java
+++ Delfinen/src/delfinen/Datamappers/DBResultat.java
@@ -1,5 +1,46 @@
 package delfinen.Datamappers;
 
-public class DBResultat {
+import delfinen.Util.DBConnector;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DBResultat {
+public static void insertTræning(int id, String diciplin , String tid, String dato) {
+        Connection MyConnector = null;
+        Statement statement = null;
+        try {
+            MyConnector = DBConnector.getConnector();
+
+            String query = "insert into medlemmers_træningsresultat values (" + id + ",'" + diciplin + "','" + tid + "','" + dato + ");";
+            statement = MyConnector.createStatement();
+            statement.executeUpdate(query);
+
+            statement.close();
+            MyConnector.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
 }
+        public static void insertKonkurrence(int id, String stævne, String diciplin , String tid, String dato, int placering) {
+        Connection MyConnector = null;
+        Statement statement = null;
+        try {
+            MyConnector = DBConnector.getConnector();
+
+            String query = "insert into medlemmers_træningsresultat values (" + id + ",'"+ stævne +"','" + diciplin + "','" + tid + "','" + dato + "'," + placering + ");";
+            statement = MyConnector.createStatement();
+            statement.executeUpdate(query);
+
+            statement.close();
+            MyConnector.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+}
+}
+
