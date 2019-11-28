0aa41752526012ba970cffdf52c7a22fa0eb6e05
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2019-11-28 15:24:55 +0100

    db

---
+++ Delfinen/src/delfinen/Datamappers/DBTræning.java
@@ -0,0 +1,26 @@
+package delfinen.Datamappers;
+
+import delfinen.Util.DBConnector;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBTræning {
+    public static void insert(String træningDato, boolean seniorTræning,boolean konkurrenceTræning) {
+        Connection MyConnector = null;
+        Statement statement = null;
+        try {
+            MyConnector = DBConnector.getConnector();
+
+            String query = "insert into træning values (null,'" + træningDato + "," + seniorTræning + ","+ konkurrenceTræning +");";
+            statement = MyConnector.createStatement();
+            statement.executeUpdate(query);
+
+            statement.close();
+            MyConnector.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+}
--- Delfinen/src/delfinen/Datamappers/InputHandler.java
+++ Delfinen/src/delfinen/Datamappers/InputHandler.java
@@ -52,3 +52,4 @@ public class InputHandler {
         return p.getYears();
     }
 }
+    
