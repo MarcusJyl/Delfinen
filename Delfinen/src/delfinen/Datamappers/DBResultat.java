339fcac6da63adc86bb4fa24c9ecb4031b387be9
Marc <marc.g.ekstrom@gmail.com>
2019-12-02 09:50:29 +0100

    case4 start

--- Delfinen/src/delfinen/Datamappers/DBResultat.java
+++ Delfinen/src/delfinen/Datamappers/DBResultat.java
@@ -2,8 +2,11 @@ package delfinen.Datamappers;
 
 import delfinen.Util.DBConnector;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class DBResultat extends DBCalls {
 
@@ -26,5 +29,46 @@ public class DBResultat extends DBCalls {
         } catch (ClassNotFoundException ex) {
             System.out.println(ex);
         }
+    }
+
+    public static String[] select() {
+        String retVal[] = new String[5];
+        Connection MyConnector = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        LocalDate date = LocalDate.now().minus(1, ChronoUnit.MONTHS);
+        String dato = date.toString();
+        System.out.println(dato);
+
+        try {
+            MyConnector = DBConnector.getConnector();
+            String query = "SELECT "
+                    + "r.trænings_id, "
+                    + "r.tid,"
+                    + "t.trænings_id, "
+                    + "t.træning_dato "
+                    + "FROM delfinen.resultater r "
+                    + "RIGHT JOIN træning t on r.trænings_id = t.trænings_id "
+                    + "where r.tid is not null and træning_dato > '" + dato + "' "
+                    + "order by r.tid;";
+            statement = MyConnector.createStatement();
+            resultSet = statement.executeQuery(query);
+
+            int i = 0;
+            while (resultSet.next() && retVal.length <= 5) {
+                retVal[i] = resultSet.getString("r.tid");
+                i++;
+            }
+
+            //lukker
+            resultSet.close();;
+            statement.close();
+            MyConnector.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+        return retVal;
     }
 }
