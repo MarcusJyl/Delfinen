c674ed9c17e72fbfd982e08e129b4746a527cf86
Marc <marc.g.ekstrom@gmail.com>
2019-12-05 13:45:13 +0100

    adasdasda

--- Delfinen/src/delfinen/Controllers/Controller.java
+++ Delfinen/src/delfinen/Controllers/Controller.java
@@ -15,51 +15,50 @@ public class Controller {
         int svar = 11;
 
         while (svar != 0) {
-            if (scanner.getInput().hasNext()) {
-                svar = scanner.getUserInteger(MainMenuUI.showMainMenu(), 9, 1);
-            
-                switch (svar) {
-                    case 0:
-                        System.exit(0);
-                        break;
-                    case 1:
-                        cases.case1();
-                        break;
-                    case 2:
-                        cases.case2();
-                        break;
-                    case 3:
-                        cases.case3();
-                        break;
-                    case 4:
-                        cases.case4();
-                        break;
-                    case 5:
-                        cases.case5();
-                        break;
-                    case 6:
-                        cases.case6();
-                        break;
-                    case 7:
-                        cases.case7();
-                        break;
-                    case 8:
-                        cases.case8();
-                        break;
-                    case 9:
-                        ArrayList<Integer> medlemsIder = scanner.getDBH().getMedlemmer(1);
-                        int i = 0;
-                        for (Integer id : medlemsIder) {
-                            i++;
-                            Medlem medlem = DBMedlemsOplysninger.medlemsOplysninger(id);
-                            System.out.println(i + ". " + medlem);
-                        }
-                        break;
 
-                }
+            svar = scanner.getUserInteger(MainMenuUI.showMainMenu(), 9, 1);
+
+            switch (svar) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    cases.case1();
+                    break;
+                case 2:
+                    cases.case2();
+                    break;
+                case 3:
+                    cases.case3();
+                    break;
+                case 4:
+                    cases.case4();
+                    break;
+                case 5:
+                    cases.case5();
+                    break;
+                case 6:
+                    cases.case6();
+                    break;
+                case 7:
+                    cases.case7();
+                    break;
+                case 8:
+                    cases.case8();
+                    break;
+                case 9:
+                    ArrayList<Integer> medlemsIder = scanner.getDBH().getMedlemmer(1);
+                    int i = 0;
+                    for (Integer id : medlemsIder) {
+                        i++;
+                        Medlem medlem = DBMedlemsOplysninger.medlemsOplysninger(id);
+                        System.out.println(i + ". " + medlem);
+                    }
+                    break;
+
             }
             System.out.println("************************************************\nSkriv hvad som helst for at gå til hoved menuen igen:");
+            scanner.getInput().nextLine();
         }
     }
-
 }
